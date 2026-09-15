6d4eb70cd6c252bc80f3bab8d1975334f644ae07
pablo <pablo@Portatil-Pablo>
2026-09-15 19:45:50 +0200

    Correccion Ejercicio 5

--- Tema01/Ejercicio05/src/ejercicio05/Principal_ej5.java
+++ Tema01/Ejercicio05/src/ejercicio05/Principal_ej5.java
@@ -1,15 +1,24 @@
 package ejercicio05;
 
+import java.util.Arrays;
+
 public class Principal_ej5 {
 
 	public static void main(String[] args) {
 		
-		int array [] = {23,6,45,67,87,34,23,65,76,2,65,67,86};
+		int array [] = new int[20];
 		int contPares = 0;
 		int contImpar = 0;
 		int mayor = Integer.MIN_VALUE;
 		int menor = Integer.MAX_VALUE;
 		
+		for(int i = 0; i<array.length;i++) {
+			int aleatorio = (int)(Math.random()*(100 - 1)) + 1;
+			array[i] = aleatorio;
+		}
+		
+		System.out.println(Arrays.toString(array));
+			
 		for(int i = 0; i< array.length;i++) {
 			if(array[i] % 2 == 0) {
 				contPares++;
