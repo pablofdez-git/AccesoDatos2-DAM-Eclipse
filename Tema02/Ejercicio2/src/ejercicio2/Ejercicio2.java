0a9ccb3e2aa27f67be41c86aff7cd10537d374a3
Alumno <Alumno@172.16.11.56>
2026-09-22 14:24:48 +0200

    Ejercicio 2 - Tema 2 -

---
+++ Tema02/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,36 @@
+package ejercicio2;
+
+import java.util.*;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			int notas [] = new int[5];
+			Random random = new Random();
+			
+			for(int i = 0; i<notas.length;i++) {
+				notas[i] = random.nextInt(10)+1;
+			}
+			
+			System.out.println("Introduce una posicion para saber la nota del alumno 0-4");
+			int pos = sc.nextInt();
+			int nota = 0;
+			
+			for(int i = 0; i<notas.length;i++) {
+				nota = notas[i];
+			}
+			
+			System.out.println("La nota del alumno es "+nota);
+		}catch(InputMismatchException e) {
+			System.out.println("El numero no es un entero");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Posicion fuera del rango del array");
+		}
+
+	}
+
+}
